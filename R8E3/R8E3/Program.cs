d3876e0b50193d9c57774477813f8a1e040c6202
Carlos Fernández Llamas <sirikon2@gmail.com>
2015-03-15 12:05:17 +0100

    Ejercicio 3

---
+++ R8E3/R8E3/Program.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.IO;
+
+namespace R8E3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length != 2)
+            {
+                Console.WriteLine("Error: Tiene que indicar dos rutas de ficheros");
+                Help();
+                return;
+            }
+            try
+            {
+                Clonar(args[0], args[1]);
+            }
+            catch (FileNotFoundException err)
+            {
+                Console.WriteLine("Error: No se pudo encontrar el fichero " + err.FileName);
+            }
+            catch
+            {
+                Console.WriteLine("Error: Sucedió un error inesperado, compruebe los argumentos");
+                Help();
+            }
+        }
+
+        static void Help()
+        {
+            Console.Write("Uso: Ejercicio3.exe <fichero1> <fichero2>");
+        }
+
+        static void Clonar(string path1, string path2)
+        {
+            FileStream f1, f2;
+            f1 = File.Open(path1, FileMode.Open, FileAccess.Read, FileShare.None);
+            f2 = File.Open(path2, FileMode.OpenOrCreate, FileAccess.Write, FileShare.None);
+
+            int b;
+            b = f1.ReadByte();
+            while (b != -1)
+            {
+                f2.WriteByte((byte)b);
+                b = f1.ReadByte();
+            }
+
+            f1.Close();
+            f2.Close();
+        }
+    }
+}
